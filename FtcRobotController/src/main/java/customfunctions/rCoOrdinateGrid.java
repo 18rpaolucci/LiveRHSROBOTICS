1284cf2b7f4aeaeb23db8c67cef6567361a4ea17
18rpaolucci <18rpaolucci@ramsey.k12.nj.us>
2016-10-05 15:17:51 -0400

    change2
    chagne2

--- FtcRobotController/src/main/java/customfunctions/rCoOrdinateGrid.java
+++ FtcRobotController/src/main/java/customfunctions/rCoOrdinateGrid.java
@@ -5,4 +5,24 @@ package customfunctions;
  */
 
 public class rCoOrdinateGrid {
+    public static int x;
+    public static int y;
+
+    public rCoOrdinateGrid(float LeftMotor, float RightMotor){
+        short lm = ((short) LeftMotor);
+        short rm = ((short) RightMotor);
+        if(LeftMotor > 0){
+            switch(lm){
+
+            }
+        }
+        if(RightMotor > 0){
+            switch(rm){
+
+            }
+        }
+
+    }
+
+
 }
