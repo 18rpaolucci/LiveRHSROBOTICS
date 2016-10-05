6546b68a34985dcca82e5addb4c13552b23e3af7
18rpaolucci <18rpaolucci@ramsey.k12.nj.us>
2016-10-05 15:28:30 -0400

    CoOrdinateSystem
    system1

---
+++ FtcRobotController/src/main/java/customfunctions/Orientation.java
@@ -0,0 +1,12 @@
+package customfunctions;
+
+/**
+ * Created by cad1 on 10/5/2016.
+ */
+
+public enum Orientation {
+    FRONT,
+    RIGHT,
+    LEFT,
+    BACK
+}
--- FtcRobotController/src/main/java/customfunctions/rCoOrdinateGrid.java
+++ FtcRobotController/src/main/java/customfunctions/rCoOrdinateGrid.java
@@ -5,20 +5,42 @@ package customfunctions;
  */
 
 public class rCoOrdinateGrid {
-    public static int x;
-    public static int y;
+    public static int x=0;
+    public static int y=0;
+    public static Orientation orientation = Orientation.FRONT;
 
     public rCoOrdinateGrid(float LeftMotor, float RightMotor){
-        short lm = ((short) LeftMotor);
-        short rm = ((short) RightMotor);
-        if(LeftMotor > 0){
-            switch(lm){
-
+        if(LeftMotor > 0 && RightMotor > 0){
+            switch(orientation){
+                case FRONT:
+                    y++;
+                    break;
+                case BACK:
+                    y--;
+                    break;
+                case RIGHT:
+                    x++;
+                    break;
+                case LEFT:
+                    x--;
+                    break;
             }
         }
-        if(RightMotor > 0){
-            switch(rm){
-
+        if(LeftMotor > 0 && RightMotor < 0){
+            //will need to compensate for timing
+            switch(orientation){
+                case FRONT:
+                    orientation = Orientation.RIGHT;
+                    break;
+                case BACK:
+                    orientation = Orientation.LEFT;
+                    break;
+                case RIGHT:
+                    orientation = Orientation.BACK;
+                    break;
+                case LEFT:
+                    orientation = Orientation.FRONT;
+                    break;
             }
         }
 
